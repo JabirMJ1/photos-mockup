b617af764c0042a6ed2bdbc9868c20ecfcb95779
JabirMJ <jabirmj1@gmail.com>
2024-06-07 18:43:47 +0530

    Hover effect on images

--- src/components/ImagesView.tsx
+++ src/components/ImagesView.tsx
@@ -4,6 +4,8 @@ import { IMAGE_SIZES } from "@/constants"
 import { TImage } from "@/types/components/types"
 import Image from "next/image"
 import { useCallback, useEffect, useState } from "react"
+import { BsCart2, BsDownload, BsImages, BsPlusCircle } from "react-icons/bs"
+import Link from 'next/link'
 
 type TImagesViewProps = {
     images: TImage[]
@@ -42,6 +44,7 @@ const ImagesView = ({images}: TImagesViewProps) => {
             <h1 className="font-bold text-sm">Cars Stock Photos and Images </h1>
             {
                 imageGroups.map((imageGroup, key) => {
+                    // adjust images to match in height and accumalated width to match screen width
                     let maxHeight = Math.max(...imageGroup.map((image) => image.height))
                     const aspectRations = imageGroup.map((image) => image.width / image.height)
                     let newWidths = imageGroup.map((image, key) => aspectRations[key] * maxHeight)
@@ -56,7 +59,24 @@ const ImagesView = ({images}: TImagesViewProps) => {
                     return <div key={key} className="relative flex space-x-2">
                         {
                             imageGroup.map((image, key1) => {
-                                return <div key = {key1} className="flex-grow">
+                                return <div key = {key1} className="relative flex-grow group" title={image.alt}>
+                                        <div className="absolute z-10 w-full h-full bg-black bg-opacity-20 hidden group-hover:flex p-2 flex-col justify-between">
+                                            <div>
+                                                <Link href={image.photographer_url} target="_blank">
+                                                    <p className="text-white truncate leading-1 max-w-full">
+                                                        <span className="bg-gray-400 text-white font-bold p-1.5 rounded aspect-square inline-block">{image.photographer.slice(0,2)}</span>
+                                                        <span className="font-bold text-sm pl-1">{image.photographer}</span> - {image.alt}
+                                                    </p>
+                                                </Link>
+                                            </div>
+
+                                            <div className="flex justify-end space-x-2 ">
+                                                <Link href={image.url} target="_blank" className="text-white bg-black bg-opacity-90 hover:bg-green-400 hover:text-black hover:bg-opacity-100 rounded p-3"><BsCart2 className="text-lg"/></Link>
+                                                <Link download target="_blank" href={image.src[IMAGE_SIZES.ORIGINAL]} className="text-white bg-black bg-opacity-90 hover:bg-green-400 hover:text-black hover:bg-opacity-100 rounded p-3"><BsDownload className="text-lg"/></Link>
+                                                <button className="text-white bg-black bg-opacity-90 hover:bg-green-400 hover:text-black hover:bg-opacity-100 rounded p-3"><BsPlusCircle className="text-lg"/></button>
+                                                <button className="text-white bg-black bg-opacity-90 hover:bg-green-400 hover:text-black hover:bg-opacity-100 rounded p-3"><BsImages className="text-lg"/></button>
+                                            </div>
+                                        </div>
                                         <Image
                                             src = {image.src[IMAGE_SIZES.ORIGINAL]}
                                             width={newWidths[key1]}
